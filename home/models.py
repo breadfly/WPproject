c8386eada29cff7455db52c835c54365862aa015
breadfly <kgh9812@naver.com>
2019-12-08 21:11:13 +0900

    change schema

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-from django.core.validators import MaxValueValidator, MinValueValidator z
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 #new
 
